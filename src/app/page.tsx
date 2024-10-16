4e049eb46b92be8cf8c492d60333f43cb1f6bf8d
Rizwana <159348120+rizwanaperveen@users.noreply.github.com>
2024-10-16 10:29:06 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return(
+    <>
+   <div className="text-center bg-black text-white text-4xl flex justify-center items-center h-screen w-screen font-serif font-extrabold">
+    <h1>"HELLO WORLD"</h1>
+    <p className="mt-24 text-2xl">This is our first milestone.</p>
+   </div>
+   <div className="text-center bg-black text-white text-4xl flex justify-center items-center h-screen w-screen font-serif font-extrabold">
+    <h2>HELLO RIZWANA</h2>
+   </div>
+    </>
+  )
+}
